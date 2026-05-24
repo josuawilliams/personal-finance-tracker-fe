5895e1bbb38a44a452f3e0cce6f3a0fe47527244
Josua Williams <josuawilliams17@gmail.com>
2026-05-25 00:20:34 +0700

    login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,44 +1,5 @@
-import { getAuth, isTokenExpired } from "@/lib/auth";
 import { Redirect } from "expo-router";
-import { useEffect, useState } from "react";
-import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const auth = await getAuth();
-
-        if (auth?.token && !isTokenExpired(auth.token)) {
-          setIsAuthenticated(true);
-        } else {
-          setIsAuthenticated(false);
-        }
-      } catch (error) {
-        console.error("Check auth error:", error);
-        setIsAuthenticated(false);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    checkAuth();
-  }, []);
-
-  if (isLoading) {
-    return (
-      <View className="flex-1 justify-center items-center bg-[#c5e8ff]">
-        <ActivityIndicator size="large" color="#6366f1" />
-      </View>
-    );
-  }
-
-  if (isAuthenticated) {
-    return <Redirect href="/(tabs)" />;
-  }
-
   return <Redirect href="/login" />;
 }
